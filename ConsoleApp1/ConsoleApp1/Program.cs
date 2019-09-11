87a8cbb7e62521fe7faec01473d93ef8c145ab94
Shival Singh <Shival.Singh@svclnk.com>
2019-09-11 05:22:46 -0400

    new changes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! This is Branch 2....");
+            Console.WriteLine("***************");
         }
     }
 }
